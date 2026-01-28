6e6fcd1eed9e46f20cda65ef0bfb599202962180
Veren Jessica <verenjessica673@gmail.com>
2026-01-28 07:19:07 +0700

    task5p4

--- app/(dashboard)/admin/categories/page.tsx
+++ app/(dashboard)/admin/categories/page.tsx
@@ -5,13 +5,53 @@ import { FiPlus } from "react-icons/fi";
 import { useState } from "react";
 import CategoryTable from "../../components/categories/category-table";
 import CategoryModal from "../../components/categories/category-modal";
+import { deleteCategory, getAllCategories } from "@/app/services/category.service";
 
 
 const CategoryManagement = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+    null,
+  );
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [categoryToDeleteId, setCategoryToDeleteId] = useState("");
+
+  const fetchCategories = async () => {
+    try {
+      const data = await getAllCategories();
+      setCategories(data)
+    } catch(error) {
+      console.error("Failed to fetch categories", error)
+    }
+  };
+
+  const handleEdit = (category: Category) => {
+    setSelectedCategory(category);
+    setIsModalOpen(true);
+  };
+
+  const handleDelete = (id: string) => {
+    setCategoryToDeleteId(id);
+    setIsDeleteModalOpen(true)
+  }
+
+  const handleDeleteConfirm = async () => {
+    if (!categoryToDeleteId) return;
+    try {
+      await deleteCategory(categoryToDeleteId);
+      fetchCategories();
+      toast.success("Category deleted successfully");
+      setIsDeleteModalOpen(false);
+      setCategoryToDeleteId("");
+    } catch (error) {
+      console.error("Failed to delete category", error);
+      toast.error("Failed to delete category");
+    }
+  };
 
   const handleCloseModal = () => {
-    setIsOpen(false);
+    setIsModalOpen(false);
   };
 
   return (
@@ -21,13 +61,13 @@ const CategoryManagement = () => {
           <h1 className="font-bold text-2xl">Category Management</h1>
           <p className="opacity-50">Organize your products into categories.</p>
         </div>
-        <Button className="rounded-lg" onClick={() => setIsOpen(true)}>
+        <Button className="rounded-lg" onClick={() => setIsModalOpen(true)}>
           <FiPlus size={24} />
           Add Category
         </Button>
       </div>
         <CategoryTable/>
-        <CategoryModal isOpen={isOpen} onClose={handleCloseModal}/>
+        <CategoryModal isModalOpen={isModalOpen} onClose={handleCloseModal}/>
     </div>
   );
 };
